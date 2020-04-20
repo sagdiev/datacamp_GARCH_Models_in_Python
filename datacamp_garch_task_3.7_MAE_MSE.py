3d1927cf39de7c1249f54eb4f6f015d151d73d15
Artem Sagdiev <sagdiev@gmail.com>
2020-04-20 15:33:00 +0300

    part 3 finalize

---
+++ datacamp_garch_task_3.7_MAE_MSE.py
@@ -0,0 +1,14 @@
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+
+def evaluate(observation, forecast):
+    # Call sklearn function to calculate MAE
+    mae = mean_absolute_error(observation, forecast)
+    print('Mean Absolute Error (MAE): {:.3g}'.format(mae))
+    # Call sklearn function to calculate MSE
+    mse = mean_squared_error(observation, forecast)
+    print('Mean Squared Error (MSE): {:.3g}'.format(mse))
+    return mae, mse
+
+# Backtest model with MAE, MSE
+evaluate(actual_var, forecast_var)
